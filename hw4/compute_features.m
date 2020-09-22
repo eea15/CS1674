d1cd91967737dcaaefb8f9ec7d68d3718034fb0f
eea15 <eea21@pitt.edu>
2020-09-22 09:48:36 -0400

    fixed part 1

--- hw4/compute_features.m
+++ hw4/compute_features.m
@@ -18,59 +18,80 @@ for i = size(x,1):-1:1
     end
 end
 
-% 2: compute gradient magnitude/angle for each (x,y)
-m = zeros(size(x,1),1); % magnitude
-d = zeros(size(x,1),1); % direction
+% 2: compute gradient magnitude/angle for each feature/neighbors
+m = zeros(size(Ix,1),size(Ix,2)); % magnitude
+d = zeros(size(Ix,1),size(Ix,2)); % direction
 
-for i = 1:size(x,1)
-    m(i,1) = sqrt((Ix(y(i),x(i)))^2 + (Iy(y(i),x(i)))^2);
+for i = 1:size(x,1) % for all keypoints
+    r = y(i); % row
+    c = x(i); % col
+    m((r-5:r+5),(c-5:c+5)) = sqrt((Ix(r-5:r+5,c-5:c+5)).^2 + (Iy(r-5:r+5,c-5:c+5).^2));
     % ignore angle if magnitude = 0
-    if(m(i,1) ~= 0)
-        d(i,1) = atand((Iy(y(i),x(i)))/(Ix(y(i),x(i))));
-    end
+     if(m((r-5:r+5),(c-5:c+5)) ~= 0) 
+         d((r-5:r+5),(c-5:c+5)) = atand((Iy(r-5:r+5,c-5:c+5))./(Ix(r-5:r+5,c-5:c+5)));
+     end
 end
 
 % 3: quantize gradient orientations in 8 bins
 o = zeros(size(Ix,1),size(Ix,2)); % matrix of orientations corresponding to image
-for i = 1:size(d,1)
-    angle = d(i);
+for i = 1:size(x,1)
+    r = y(i); % row
+    c = x(i); % col
     
-    if(angle >= -90 && angle < -67.5)
-        o(y(i),x(i)) = 1;
-    elseif(angle >= -67.5 && angle < -45)
-        o(y(i),x(i)) = 2;
-    elseif(angle >= -45 && angle < -22.5)
-        o(y(i),x(i)) = 3;
-    elseif(angle >= -22.5 && angle < 0)
-        o(y(i),x(i)) = 4;
-    elseif(angle >= 0 && angle < 22.5)
-        o(y(i),x(i)) = 5;
-    elseif(angle >= 22.5 && angle < 45)
-        o(y(i),x(i)) = 6;
-    elseif(angle >= 45 && angle < 67.5)
-        o(y(i),x(i)) = 7;
-    elseif(angle >= 67.5 && angle <= 90)
-        o(y(i),x(i)) = 8;
-    else
-        fprintf("error: invalid angle");
+    % go thru 11x11 window
+    for p = r-5:r+5
+        for q = c-5:c+5
+            angle = d(r,c);
+            % label 1-8
+            if(angle >= -90 && angle < -67.5)
+                o(p,q) = 1;
+            elseif(angle >= -67.5 && angle < -45)
+                o(p,q) = 2;
+            elseif(angle >= -45 && angle < -22.5)
+                o(p,q) = 3;
+            elseif(angle >= -22.5 && angle < 0)
+                o(p,q) = 4;
+            elseif(angle >= 0 && angle < 22.5)
+                o(p,q) = 5;
+            elseif(angle >= 22.5 && angle < 45)
+                o(p,q) = 6;
+            elseif(angle >= 45 && angle < 67.5)
+                o(p,q) = 7;
+            elseif(angle >= 67.5 && angle <= 90)
+                o(p,q) = 8;
+            else
+                fprintf("error: invalid angle");
+            end
+        end
     end
 end
 
 % 4: populate the SIFT histogram
-hist = zeros(1,8);
-for i = 1:size(d,1)
-    bin = o(y(i),x(i));
-    hist(1,bin) = hist(1,bin) + m(i);
+hist = zeros(size(x,1),8);
+for i = 1:size(x,1)
+    r = y(i); % row
+    c = x(i); % col
+    
+    for p = r-5:r+5 % go thru feature windows
+        for q = c-5:c+5
+            bin = o(p,q);
+            hist(i,bin) = hist(i,bin) + m(p,q);
+        end
+    end
 end
 
 % 5: normalize and clip
-hist = hist/sum(hist);
-for i = 1:size(hist)
-    if(hist(i) > 0.2)
-        hist(i) = 0.2
+for i = 1:size(hist,1)
+     hist(i,:) = hist(i,:)/sum(hist(i,:));
+end
+for i = 1:size(hist,1)
+    for j = 1:8
+        if(hist(i,j) > 0.2)
+            hist(i,j) = 0.2;
+        end
     end
 end
-hist = hist/sum(hist);
+for i = 1:size(hist,1)
+     hist(i,:) = hist(i,:)/sum(hist(i,:));
+end
 
-% next up: fix above to implement as window over each feature
-% see slide 68 :)
